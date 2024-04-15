a23f135657108e276b3c86906985bad69e970f01
szmisztal <sz.misztal@gmail.com>
2024-04-15 13:19:26 +0200

    add Session class that define user session after log in

---
+++ server/Data/Session.cs
@@ -0,0 +1,17 @@
+﻿public class Session
+{
+    public bool IsAuthenticated { get; set; } = false;
+    public string Username { get; set; } = null;
+
+    public void Authenticate(string username)
+    {
+        IsAuthenticated = true;
+        Username = username;
+    }
+
+    public void Logout()
+    {
+        IsAuthenticated = false;
+        Username = null;
+    }
+}
